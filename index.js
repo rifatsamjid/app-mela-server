35f6c94505d6fdd1c568af08d2c01d5872d96f80
rifatsamjid <rifattamim74328@gmail.com>
2025-12-03 13:59:48 +0600

    server connect to mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 4000
+require("dotenv").config();
 
 app.use(cors())
 app.use(express.json())
 
+
+
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS
+
+
+const uri = `mongodb+srv://${user}:${pass}@cluster1.jkfjkqt.mongodb.net/?appName=Cluster1`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+    try{
+        await client.connect();
+        await client.db("admin").command({ping: 1})
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    }
+    finally{
+        
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send("Hello This is my app mela server")
